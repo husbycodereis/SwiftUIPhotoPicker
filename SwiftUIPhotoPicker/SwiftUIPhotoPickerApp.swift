83514fa9a3f4226f11acd2b70bcdfabebf93f5ad
Ali Rıza Reisoğlu <alirizareisoglu@gmail.com>
2022-04-13 15:28:36 +0300

    Initial Commit

---
+++ SwiftUIPhotoPicker/SwiftUIPhotoPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPhotoPickerApp.swift
+//  SwiftUIPhotoPicker
+//
+//  Created by Ali Riza Reisoglu on 13.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPhotoPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
